cd92340add45ce1cbba07503aa30f490e617900c
Shin HyeJi <jssin97@gmail.com>
2024-02-14 13:28:08 +0900

    240214: Init a data strcuture with table

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,19 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+P
+ src/main/java/domain/Member.java,8/a/8a5d22a7ae5a65bb4ee339d7bd18d9fdd4fd89ef
+_
+/src/main/java/jpabook/jpasho/domain/Member.java,6/3/633f930b182d7538a040c834f65ec794b8c3dbd8
+`
+0src/main/java/jpabook/jpashop/domain/Member.java,4/b/4bf907111baa3183e3e5fe7c9be862ec7da142f7
+_
+/src/main/java/jpabook/jpashop/domain/Order.java,b/e/be11ca5f753c33204b340512509491ae2239cb4b
+c
+3src/main/java/jpabook/jpashop/domain/OrderItem.java,5/f/5ff98ff609f00aa390c3badb12b283b172996795
+^
+.src/main/java/jpabook/jpashop/domain/Item.java,6/0/60e3c8697fd0b451189ded1f0935445468acce54
+e
+5src/main/java/jpabook/jpashop/domain/OrderStatus.java,6/2/6263648f9b5a55943c09d979922e8aaebe8b7604
+Z
+*src/main/java/jpabook/jpashop/JpaMain.java,d/8/d8078389fd62838fd61e9a23df1bf84f0a19134a
---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); // 트랜젝션 연결
+
+        try {
+            Order order = em.find(Order.class, 1L);
+            Long memberId = order.getMemberId();
+
+            Member findMember = order.getMember();
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
